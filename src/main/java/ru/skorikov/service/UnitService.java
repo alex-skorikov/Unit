065a383af5752bdb79fe9c0c3f7a3aa1bc13db76
alex-skorikov <the.na4ig@gmail.com>
2020-03-02 16:25:06 +1000

    added dounload from file,test

--- src/main/java/ru/skorikov/service/UnitService.java
+++ src/main/java/ru/skorikov/service/UnitService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.skorikov.domain.Unit;
 import ru.skorikov.repository.UnitRepository;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
